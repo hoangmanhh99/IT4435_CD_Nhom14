29af741e25b67e938b8436345e01e9b84918f6f6
Tuan.LA <luonganhtuan1051999@gmail.com>
2021-05-08 17:47:40 +0700

    fix validate login, register for user and admin

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,47 +1,53 @@
-const {body, validationResult} = require('express-validator');
-const CustomError = require('../errors/CustomError')
-const errorCodes = require('../errors/code');
-const apiTypes = {
-    LOGIN: 'login',
-    REGISTER: 'register'
+//const {body, validationResult} = require('express-validator');
+//Validation
+const Joi = require('joi');
 
+// const CustomError = require('../errors/CustomError')
+// const errorCodes = require('../errors/code');
+// const apiTypes = {
+//     LOGIN: 'login',
+//     REGISTER: 'register'
+
+// }
+
+const loginValidation = data => {
+    const schema = Joi.object({
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required() //.pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+    })
+
+    return schema.validate(data) ;
 }
 
-const validate = (api) => {
-    switch (api) {
-        case apiTypes.LOGIN:
-            return [
-                body('email')
-                    .exists()
-                    .withMessage('"email" is required')
-                    .isEmail()
-                    .withMessage('"email" is invalid'),
-                body('password')
-                    .exists()
-                    .withMessage('"password" is required')
-                    .isString()
-                    .trim()
-            ];
-        default:
-            return [];
+// Register validation 
+const registerValidation = data => {
+    const schema = Joi.object({
+        name: Joi.string().min(6).required(),
+        email: Joi.string().min(6).required().email(),
+        password: Joi.string().min(6).required() //.pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+    })
 
-    }
-
+    return schema.validate(data) ;
 }
 
-const getValidateResult = (req, res, next) => {
-    const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-      let msgs = errors.array().map(element => element.msg);
-      let message = msgs.toString();
-    throw new CustomError(errorCodes.BAD_REQUEST, message, errors.array().shift().msg);
-  }
-  next();
-}
+// const getValidateResult = (req, res, next) => {
+//     const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//       let msgs = errors.array().map(element => element.msg);
+//       let message = msgs.toString();
+//     throw new CustomError(errorCodes.BAD_REQUEST, message, errors.array().shift().msg);
+//   }
+//   next();
+// }
 
 
-module.exports = {
-    getValidateResult,
-    validate,
-    apiTypes
+// module.exports = {
+//     getValidateResult,
+//     validate,
+//     apiTypes
+// }
+
+module.exports={
+    registerValidation, 
+    loginValidation 
 }
